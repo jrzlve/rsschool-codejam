0eda088ab56f45e1d94dbea80679d0a26824623a
jrzlve <jrzlve@gmail.com>
2018-10-27 21:28:14 +0300

    docs : sumOfOther function v.1

---
+++ sumOfOther.js
@@ -0,0 +1,7 @@
+function sumOfOther(arr){
+    let sum = 0;
+    const result = [];
+    for (let i = 0, len = arr.length; i < len; i++) sum += arr[i];
+    for (let i = 0, len = arr.length; i < len; i++) result.push(sum - arr[i]);
+    return result;
+}
